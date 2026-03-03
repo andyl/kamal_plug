2724fad3d462ea585a2a65e97c2da1187ae70742
andyl <andy@r210.com>
2026-03-02 21:01:12 -0800

    Add shortdoc

--- lib/mix/tasks/kamal_plug/install.ex
+++ lib/mix/tasks/kamal_plug/install.ex
@@ -1,7 +1,7 @@
 defmodule Mix.Tasks.KamalPlug.Install do
   use Igniter.Mix.Task
 
-  # Adds KamalPlug.HealthCheck to your Phoenix endpoint
+  @shortdoc "Adds KamalPlug.HealthCheck to your Phoenix endpoint"
 
   @impl Igniter.Mix.Task
   def info(_argv, _composing_task) do
